4cad1baedd1f66e06ba9fb6232ae029a1de1f8de
Franco Micheloud <TUMAIL@gmail.com>
2026-05-19 14:34:23 -0300

    fix sidebar structure cleanup

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,33 +2,17 @@
 
 import "./globals.css";
 
-import Sidebar
-from "../../components/layout/Sidebar";
+import Sidebar from "@/components/layout/Sidebar";
 
-export default function RootLayout({
-  children,
-}) {
-
+export default function RootLayout({ children }) {
   return (
-
     <html lang="es">
-
       <body className="bg-gray-100">
-
         <div className="flex">
-
           <Sidebar />
-
-          <main className="flex-1 p-6">
-
-            {children}
-
-          </main>
-
+          <main className="flex-1 p-6">{children}</main>
         </div>
-
       </body>
-
     </html>
   );
 }
