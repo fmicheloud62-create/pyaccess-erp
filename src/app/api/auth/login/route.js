a5d53ddde140d526c2a37ec983eb72b229cc4425
Franco Micheloud <TUMAIL@gmail.com>
2026-05-19 20:31:51 -0300

    test minimal api

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,105 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-
-import bcrypt from "bcryptjs";
-
-import jwt from "jsonwebtoken";
-
-const prisma = new PrismaClient();
-
-const SECRET = "PYACCESS_SECRET";
-
 export const dynamic = "force-dynamic";
 
 export const runtime = "nodejs";
 
-export async function GET() {
+export async function POST(req) {
 
-}
+  return Response.json({
+    success: true
+  });
 
-export async function POST(request) {
-
-  try {
-
-    const body =
-      await request.json();
-
-    const user =
-      await prisma.user.findUnique({
-
-        where: {
-          username:
-            body.username,
-        },
-      });
-
-    if (!user) {
-
-      return Response.json(
-        {
-          error:
-            "Usuario no encontrado",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
-
-    const valid =
-      await bcrypt.compare(
-        body.password,
-        user.password
-      );
-
-    if (!valid) {
-
-      return Response.json(
-        {
-          error:
-            "Contraseña incorrecta",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
-
-    const token =
-      jwt.sign(
-        {
-          id: user.id,
-          role: user.role,
-        },
-        SECRET,
-        {
-          expiresIn: "7d",
-        }
-      );
-
-    return Response.json({
-
-      token,
-
-      user: {
-        id: user.id,
-        username:
-          user.username,
-        role: user.role,
-      },
-    });
-
-  } catch (error) {
-
-    console.log(error);
-
-    return Response.json(
-      {
-        error:
-          "Error interno",
-      },
-      {
-        status: 500,
-      }
-    );
-  }
 }
